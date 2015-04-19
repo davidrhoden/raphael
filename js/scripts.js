277f9a7431acc735197103ecb93881a9b30b522b
David Rhoden <david@davidrhoden.com>
2015-04-18 22:48:06 -0500

    a change

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ ring.attr({stroke:"#000", "stroke-width":5, "stroke-dasharray": "-"});
 var circle1 = paper.circle(200, 48, 16);
 circle1.attr({"fill":"yellow", "stroke":"#ffff00"}); 
 var circle2 = paper.circle(350, 96, 16);
-circle2.attr({"fill":"hotpink", "stroke":"#ffffoo"});  
+circle2.attr({"fill":"hotpink", "stroke":"#ffff00"});  
 var circle3 = paper.circle(350, 96, 16);
 circle3.attr({"fill":"orange", "stroke":"#ffff00"}); 
 var circle4 = paper.circle(200, 48, 16);
@@ -19,18 +19,18 @@ window.onload = function() {
     animation = window.setInterval("animateCircles()", 10);  
 };
 
-// circle2.node.onclick = function(){
-//     integrity.animate({opacity: 1}, 2000);
-// };
 
 var counter = 0; 
 function animateCircles(){
     if((ring.getTotalLength()/5) <= counter){ 
         clearInterval(animation);
+                var honesty = paper.text(200, 100, "Honesty\nIntegrity\nNot Lying").attr({'text-anchor': 'start'});
+        var integrity = paper.text(380, 180, "Integrity\nCooperation\nhelpingness").attr({'text-anchor': 'start', 'cursor': 'pointer', 'opacity': '.5'});
+        circle2.node.onclick = function(){
+    integrity.animate({opacity: 1}, 2000);
+};
         return;
     }
-        var honesty = paper.text(200, 100, "Honesty\nIntegrity\nNot Lying").attr({'text-anchor': 'start'});
-        var integrity = paper.text(380, 180, "Integrity\nCooperation\nhelpingness").attr({'text-anchor': 'start', 'cursor': 'pointer', 'opacity': '.5'});
         var pos1 = ring.getPointAtLength(counter);   //get the position (see Raphael docs)
         var pos2 = ring.getPointAtLength(counter*2);  
         var pos3 = ring.getPointAtLength(counter*3);  
@@ -42,4 +42,4 @@ function animateCircles(){
         circle4.attr({cx: pos4.x, cy: pos4.y, "cursor": "pointer"});
         circle5.attr({cx: pos5.x, cy: pos5.y, "cursor": "pointer"});
         counter++;
-};
+ };
